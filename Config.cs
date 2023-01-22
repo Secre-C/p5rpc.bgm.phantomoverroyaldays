81ab96e5696e6db17d7d13110bf737e6a4980f8a
SecreC <gabirocks505@gmail.com>
2023-01-21 18:16:20 -0800

    Add project files.

---
+++ Config.cs
@@ -0,0 +1,42 @@
+﻿using p5rpc.bgm.phantomoverroyaldays.Template.Configuration;
+using System.ComponentModel;
+
+namespace p5rpc.bgm.phantomoverroyaldays.Configuration
+{
+    public class Config : Configurable<Config>
+    {
+        /*
+            User Properties:
+                - Please put all of your configurable properties here.
+
+            By default, configuration saves as "Config.json" in mod user config folder.    
+            Need more config files/classes? See Configuration.cs
+
+            Available Attributes:
+            - Category
+            - DisplayName
+            - Description
+            - DefaultValue
+
+            // Technically Supported but not Useful
+            - Browsable
+            - Localizable
+
+            The `DefaultValue` attribute is used as part of the `Reset` button in Reloaded-Launcher.
+        */
+
+        [DisplayName("Title Screen BGM Cue")]
+        [Description("Sets the BGM Cue that plays during the title screen")]
+        [DefaultValue((short)0x65)]
+        public short cue { get; set; } = 0x65;
+    }
+
+    /// <summary>
+    /// Allows you to override certain aspects of the configuration creation process (e.g. create multiple configurations).
+    /// Override elements in <see cref="ConfiguratorMixinBase"/> for finer control.
+    /// </summary>
+    public class ConfiguratorMixin : ConfiguratorMixinBase
+    {
+        // 
+    }
+}
---
+++ Phantom.cs
@@ -0,0 +1,33 @@
+﻿using Reloaded.Hooks.Definitions;
+using Reloaded.Mod.Interfaces;
+using Reloaded.Memory.SigScan.ReloadedII.Interfaces;
+using Reloaded.Memory.Sources;
+using System.Diagnostics;
+using System.Collections;
+using System;
+using p5rpc.bgm.phantomoverroyaldays.Configuration;
+
+namespace p5rpc.noholdupmusic
+{
+    public unsafe class Phantom
+    {
+        public Phantom(IReloadedHooks hooks, ILogger logger, IModLoader modLoader, Config configuration)
+        {
+            var memory = Memory.Instance;
+
+            using var thisProcess = Process.GetCurrentProcess();
+            long baseAddress = thisProcess.MainModule.BaseAddress.ToInt64();
+
+            modLoader.GetController<IStartupScanner>().TryGetTarget(out var startupScanner);
+
+            startupScanner.AddMainModuleScan("B9 85 03 00 00 66 89 56 ??", (result) =>
+            {
+                long holdupMusicStart = result.Offset + baseAddress;
+                if (result.Found)
+                {
+                    memory.SafeWrite(holdupMusicStart + 1, configuration.cue);
+                }
+            });
+        }
+    }
+}
